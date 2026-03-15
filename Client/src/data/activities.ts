d2f62c5bc5a70c329ebcd33e92274d61c2b4d447
inghamj1 <inghamj1@newpaltz.edu>
2026-03-15 17:56:35 -0400

    Fixed Images

--- Client/src/data/activities.ts
+++ Client/src/data/activities.ts
@@ -42,7 +42,7 @@ export const activities = ref<Activity[]>([
     calories: 150,
     date: '2026-03-14',
     imageUrl:
-      'https://images.unsplash.com/photo-1571008887538-b36bb32f4571?q=80&w=1740&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D',
+      'https://plus.unsplash.com/premium_photo-1661777196224-bfda51e61cfd?q=80&w=1740&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D',
   },
   {
     id: 5,
@@ -52,7 +52,7 @@ export const activities = ref<Activity[]>([
     calories: 200,
     date: '2026-03-15',
     imageUrl:
-      'https://images.unsplash.com/photo-1571008887538-b36bb32f4571?q=80&w=1740&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D',
+      'https://images.unsplash.com/photo-1517836357463-d25dfeac3438?q=80&w=1740&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D',
   },
   {
     id: 6,
