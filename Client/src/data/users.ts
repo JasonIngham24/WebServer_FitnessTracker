752bfc05a86a0684af90ce97892f5e6c9687f49f
inghamj1 <inghamj1@newpaltz.edu>
2026-03-15 17:29:34 -0400

    Updated Users

--- Client/src/data/users.ts
+++ Client/src/data/users.ts
@@ -1,31 +1,31 @@
-import type { User } from '../models/user';
+import type { User } from '../models/user'
 
 export const users: User[] = [
-    {
-        id: 1,
-        role: 'admin',
-        firstName: 'Admin',
-        lastName: 'User',
-        username: 'admin',
-        email: 'admin@example.com',
-        friends: [2, 3],
-    },
-    {
-        id: 2,
-        role: 'user',
-        firstName: 'User',
-        lastName: 'One',
-        username: 'user1',
-        email: 'user1@example.com',
-        friends: [1, 3],
-    },
-    {
-        id: 3,
-        role: 'user',
-        firstName: 'User',
-        lastName: 'Two',
-        username: 'user2',
-        email: 'user2@example.com',
-        friends: [1, 2],
-    },
-];
+  {
+    id: 1,
+    role: 'admin',
+    firstName: 'Jason',
+    lastName: 'Ingham',
+    username: 'Inghamj1',
+    email: 'admin@example.com',
+    friends: [2, 3],
+  },
+  {
+    id: 2,
+    role: 'user',
+    firstName: 'Smitty',
+    lastName: 'WerbenJagerManJensen',
+    username: 'The_#1',
+    email: 'user1@example.com',
+    friends: [1, 3],
+  },
+  {
+    id: 3,
+    role: 'user',
+    firstName: 'Bread',
+    lastName: 'Pitt',
+    username: 'Not_BradPitt',
+    email: 'user2@example.com',
+    friends: [1, 2],
+  },
+]
