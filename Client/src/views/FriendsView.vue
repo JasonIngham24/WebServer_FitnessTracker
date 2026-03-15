f5796106362a6ee07000dac9eea66fa326e4d68a
inghamj1 <inghamj1@newpaltz.edu>
2026-03-15 17:33:18 -0400

    Made Friends and Users view consistant

--- Client/src/views/FriendsView.vue
+++ Client/src/views/FriendsView.vue
@@ -1,35 +1,51 @@
 <script setup lang="ts">
-import { getMyFriends } from '@/services/session';
-import { activities } from '@/data/activities';
+import { getMyFriends } from '@/services/session'
+import { activities } from '@/data/activities'
 
-const friends = getMyFriends();
+const friends = getMyFriends()
 
 function getFriendActivities(friendId: number) {
-    return activities.value.filter((a) => a.userId === friendId);
+  return activities.value.filter((a) => a.userId === friendId)
 }
 </script>
 
 <template>
-    <div class="container">
-        <h1 class="title">Friends</h1>
-        <div v-for="friend in friends" :key="friend.id">
-            <h2 class="subtitle">{{ friend.username }}'s Activities</h2>
-            <table class="table is-fullwidth">
-                <thead>
-                    <tr>
-                        <th>Activity</th>
-                        <th>Duration (minutes)</th>
-                        <th>Date</th>
-                    </tr>
-                </thead>
-                <tbody>
-                    <tr v-for="activity in getFriendActivities(friend.id)" :key="activity.id">
-                        <td>{{ activity.activity }}</td>
-                        <td>{{ activity.duration }}</td>
-                        <td>{{ activity.date }}</td>
-                    </tr>
-                </tbody>
-            </table>
+  <div class="container">
+    <h1 class="title">Friends' Activities</h1>
+    <div v-for="friend in friends" :key="friend.id">
+      <div v-for="activity in getFriendActivities(friend.id)" :key="activity.id" class="card">
+        <div class="card-content">
+          <div class="media">
+            <div class="media-left"></div>
+            <div class="media-content">
+              <p class="title is-4">{{ friend.firstName }} {{ friend.lastName }}</p>
+              <p class="subtitle is-6">@{{ friend.username }}</p>
+              <div class="card-image" v-if="activity.imageUrl">
+                <figure class="image">
+                  <img :src="activity.imageUrl" alt="Activity image" class="activity-image" />
+                </figure>
+              </div>
+            </div>
+          </div>
+          <div class="content">
+            {{ activity.activity }}
+            <br />
+            <strong>Duration:</strong> {{ activity.duration }} minutes
+            <br />
+            <time :datetime="activity.date">{{ activity.date }}</time>
+          </div>
         </div>
+      </div>
     </div>
+  </div>
 </template>
+
+<style scoped>
+.card {
+  margin-top: 1rem;
+}
+.activity-image {
+  object-fit: cover;
+  max-height: 100em;
+}
+</style>
